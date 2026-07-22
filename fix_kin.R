00cd914522c4557ad6b20b876ab8969ac53dee92
Omar Lizardo <omarlizardo@gmail.com>
2026-07-22 08:36:05 -0700

    poof

--- fix_kin.R
+++
@@ -1,4 +0,0 @@
-lines <- readLines("R/kin_script.R")
-lines <- gsub("writeLines(as.character.*", "", lines, fixed=TRUE)
-lines <- gsub("output = 'latex'", "output = here('Tabs', 'pcs_kin_comparison.tex')", lines, fixed=TRUE)
-writeLines(lines, "R/kin_script.R")
--- fix_script.R
+++
@@ -1,4 +0,0 @@
-lines <- readLines("R/pcs-analysis-modern.R")
-lines <- gsub("writeLines(as.character.*", "", lines, fixed=TRUE)
-lines <- gsub("output = 'latex'", "output = here('Tabs', 'pcs_taste_change_modern.tex')", lines)
-writeLines(lines, "R/pcs-analysis-modern.R")
